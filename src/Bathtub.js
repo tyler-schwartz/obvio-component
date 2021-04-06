5671300193451f232cafee755d33066058f56a51
tylers <tylers@nationalfibre.net>
2021-04-06 10:29:17 -0400

    Refactored to simplify; Addressing Mike's concerns

--- src/Bathtub.js
+++ src/Bathtub.js
@@ -1,80 +1,88 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Bathtub(props)
 {
     const MAX_LEVEL     = 5;
-    const DEFAULT_DELAY = 2;
-    const interval      = useRef(null);
+    const DEFAULT_DELAY = .1;
 
-    const [levels, setLevels]         = useState([]);
+    const [level, setLevel]           = useState(0);
     const [direction, setDirection]   = useState(null);
     const [waterLevel, setWaterLevel] = useState(MAX_LEVEL);
     const [delay, setDelay]           = useState(DEFAULT_DELAY);
+    const [tick, setTick]             = useState(0);
 
-    // Gonna do some work anytime the direction state value changes.
     useEffect(() => {
-        // If it's null, we're either initializing or the work is done. In either
-        // of these cases we don't want to do anything on the state change to null.
+        // If direction is null, we're either initializing or the work is done.
+        // In either of these cases we don't want to do anything on the state,
+        // so change to null and be done.
         if (direction === null) { return; }
 
-        // Before any work is done, if we're being told to up and we're already
-        // at the desired level, reset the direction and be done.
-        if (direction === 'up' && levels.length === waterLevel) {
+        // If we're going up and the level is already at the desired level; OR...
+        // we're going down and the level is at 0 (empty tub), we're done working,
+        // so set the direction to null (our "do work" indicator) and split.
+        if ((direction === 'up' && level >= Number(waterLevel)) ||
+            (direction === 'down' && level === 0)
+        ) {
             setDirection(null);
             return;
         }
 
-        // Change the level and set it to the state so that user sees something
-        // happening. If the delay is longer, it takes some time before any visual
-        // indication of work is happening. Creating a new array, so that we can
-        // trigger a render of the component after the state commit.
-        doChangeLevels();
-        setLevels(Object.assign([], levels));
-
-        // Set the ref to the setInterval() id so we have something to clear when
-        // the work is done.
-        interval.current = setInterval(() => {
-            // Depending on the direction, push or pop an element from the array
-            // of water levels. Array? Easier to .map() and allows for some future-
-            // proofing if it's a collection of items, opposed to just a level.
+        // In this case, we're going down and we're looking for a mid-way stop.
+        // We need to check the current level - 1, to save having to have another
+        // variable for comparison. Because we're "stopping" when the current
+        // level - 1 is equal to our desired waterLevel, we still have to change
+        // the level one more time so that that the mid-way stop happens on the
+        // correct display, not desired waterLevel + 1. Again, change the direction
+        // to null and get out of here.
+        if (direction === 'down' && (level - 1) === Number(waterLevel)) {
             doChangeLevels();
+            setDirection(null);
+            return;
+        }
 
-            // Three cases where we want to stop working:
-            // 1. We're moving the level up and the length of the water level
-            //    array is at the desired level;
-            // 2. We're moving down and the length of the array is at the desired
-            //    level - this handles the case of starting from "full up" and
-            //    ending at "half-way";
-            // 3. We're moving down and the length of the array is 0, we've
-            //    removed everything and the tub is empty.
-            if ((direction === 'up' && levels.length >= Number(waterLevel)) ||
-                (direction === 'down' && levels.length === Number(waterLevel)) ||
-                (direction === 'down' && levels.length === 0)
-            ) {
-                // Don't want to keep iterating...
-                clearInterval(interval.current);
-                // Creating a new array, so that we can trigger a render of the
-                // component after the state commit.
-                setLevels(Object.assign([], levels));
-                // Need to make sure we change the direction to null, so that we
-                // can capture the "do work" signal again after.
-                setDirection(null);
-                // We're done, so get out of here.
-                return;
-            }
+        // At this point, we've figured out we're not stopping work, we're starting
+        // it. SO, change the levels to get the instant gratification of something
+        // happening on the screen.
+        doChangeLevels();
 
-            // At this point, levels has "local" changes but hasn't been saved to
-            // state yet. Creating a new array, so that we can trigger a render
-            // of the component after the state commit.
-            setLevels(Object.assign([], levels));
+        // Use setTimeout() to increment a tick state variable. That is the only
+        // work we're doing in the timeout, the useEffect() is watching the tick
+        // state to fire again. Since all the stopping logic happens before the
+        // timeout is triggered and because we want the instant gratification,
+        // the changing of levels can't happen inside the timeout.
+        setTimeout(() => {
+            setTick(tick => tick + 1);
         }, Number(delay) * 1000);
-    }, [direction]);
+    }, [direction, tick]);
 
+    /**
+     * Check which direction we're going and math the levels into that direction.
+     * Common place to do same logic multiple times.
+     */
     const doChangeLevels = () => {
-        direction === 'up' ?
-            levels.push({}) :
-            levels.pop();
+        setLevel(direction === 'up' ?
+            level + 1 :
+            level - 1
+        );
     };
+
+    /**
+     * Iterates the number of levels we're currently sitting on and pushes a div
+     * into an array. That array gets returend and rendered.
+     * @returns array
+     */
+    const doRenderWater = () => {
+        let waterLevels = [];
+
+        for (let i=0; i<level; i++) {
+            waterLevels.push(<div
+                className={`bathtub-waterlevel level-${i}`}
+                key={i}
+            />);
+        }
+
+        return waterLevels;
+    }
 
     return (
         <div className="bathtub-container">
@@ -120,17 +128,12 @@ function Bathtub(props)
             </section>
 
             <div className="bathtub-tub">
-                {levels.map((item, key) => {
-                    return <div
-                        className={`bathtub-waterlevel level-${key}`}
-                        key={key}
-                    />
-                })}
+                {doRenderWater()}
             </div>
 
             <div className="bathtub-info">
                 <span>Direction: {direction || '--'}</span>
-                <span>Level: {levels.length}</span>
+                <span>Level: {level}</span>
             </div>
 
             <p>
